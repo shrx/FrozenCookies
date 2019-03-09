77055a432dcf0d88b5110d35a6080b82b45f728f
shrx <differenxe@gmail.com>
2019-03-09 11:10:46 +0100

    temporary selfost of patch

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -4,7 +4,7 @@ var scriptElement = document.getElementById('frozenCookieScript') !== null ?
     document.getElementById('modscript_frozen_cookies');
 var baseUrl = scriptElement !== null ?
     scriptElement.getAttribute('src').replace(/\/frozen_cookies\.js$/, '') :
-    'https://mtarnuhal.github.io/FrozenCookies/';
+    'https://github.com/shrx/FrozenCookies/blob/temporary-selfhost/';
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
